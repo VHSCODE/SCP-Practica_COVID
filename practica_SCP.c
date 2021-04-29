dde1168dd88854d03cb2908f4a34c7f3d6017562
VHSCODE <shadowjosebau@gmail.com>
2021-04-29 12:05:20 +0200

    Implementada la infeccion de las personas

--- practica_SCP.c
+++ practica_SCP.c
@@ -3,14 +3,18 @@
 #include "time.h"
 #include "assert.h"
 
-
 /**
  * Utilidad que simula la expansion del Virus T en la poblacion de Raccoon City. 
  * 
  * Desarrollado por la Corporacion Umbrella
  * 
  **/
-#define DEBUG 1 //Usado durante el desarrollo para diferenciar entre diferentes personas.
+
+
+
+//#define DEBUG 1 //Usado durante el desarrollo para diferenciar entre diferentes personas.
+
+
 //######################### Definiciones de tipos #########################
 
 enum Estado
@@ -36,20 +40,20 @@ typedef struct
     double probabilidad_muerte; // Usado en caso de estar infectado
     Tupla posicion;             //Posicion x e y dentro del mundo
     Tupla velocidad;            // Representa el movimiento a realizar en cada eje. X e Y en nuestro caso.
-    #ifdef DEBUG
+#ifdef DEBUG
     int identificador;
-    #endif
+#endif
 } Persona;
 
 //##################################################
 
 //######################### Constantes #########################
 
-#define TAMANNO_MUNDO 10    //Tamaño de la matriz del mundo
-#define TAMANNO_POBLACION 20 //Cantidad de personas en el mundo
+#define TAMANNO_MUNDO 500    //Tamaño de la matriz del mundo
+#define TAMANNO_POBLACION 1000 //Cantidad de personas en el mundo
 
 #define VELOCIDAD_MAX 5 //Define la velocidad maxima a la que una persona podra viajar por el mundo. El numero indica el numero de "casillas"
-
+#define RADIO 25        //Define el radio de infeccion de una persona infectada
 //##################################################
 
 void dibujar_mundo(Persona **mundo);
@@ -57,8 +61,9 @@ int comprobar_posicion(const Tupla posiciones_asignadas[TAMANNO_POBLACION], cons
 void inicializar_mundo(Persona **mundo);
 void simular_ciclo(Persona **mundo);
 int random_interval(int min, int max);
-
-
+Tupla mover_persona(Persona **mundo, Persona *persona);
+void recoger_metricas(Persona** mundo);
+void infectar(Persona **mundo, Tupla posicion_persona);
 
 int main(int argc, char const *argv[])
 {
@@ -66,16 +71,13 @@ int main(int argc, char const *argv[])
 
     srand(time(NULL));
 
-
-    if(argc < 2)
+    if (argc < 2)
     {
         printf("Uso: ./practica_scp <numero_de_iteraciones>\n");
         return 0;
     }
 
-
     long iteraciones = atol(argv[1]); //Numero de iteraciones a realizar.
-
 
     Persona **mundo = malloc(TAMANNO_MUNDO * TAMANNO_MUNDO * sizeof(Persona *)); // "Tablero" que representara el mundo en una matriz 2D
     if (mundo == NULL)
@@ -86,31 +88,34 @@ int main(int argc, char const *argv[])
 
     inicializar_mundo(mundo);
 
+    #ifdef DEBUG
     printf("Situacion inicial\n");
 
     dibujar_mundo(mundo);
-    
-    
+    #endif
     int iteraciones_realizadas = 0;
 
     for (iteraciones_realizadas; iteraciones_realizadas < iteraciones; iteraciones_realizadas++)
     {
         simular_ciclo(mundo);
 
-        printf("Iteracion %d: \n",iteraciones_realizadas + 1);
+        #ifdef DEBUG
+        printf("Iteracion %d: \n", iteraciones_realizadas + 1);
+        
         dibujar_mundo(mundo);
+        #endif
     }
 
     //TODO Recoger metricas de la simulacion: Cantidad de muertes, supervivientes, posiciones de los mismos, etc...
 
-
+    recoger_metricas(mundo);
     //Liberamos la memoria asignada
     int i, j;
     for (i = 0; i < TAMANNO_MUNDO; i++)
     {
         for (j = 0; j < TAMANNO_MUNDO; j++)
         {
-            if(mundo[i +j * TAMANNO_MUNDO] != NULL)
+            if (mundo[i + j * TAMANNO_MUNDO] != NULL)
                 free(mundo[i + j * TAMANNO_MUNDO]);
         }
     }
@@ -124,49 +129,107 @@ int main(int argc, char const *argv[])
  **/
 void simular_ciclo(Persona **mundo)
 {
-    int i,j;
+    int i, j;
     for (i = 0; i < TAMANNO_MUNDO; i++)
     {
         for (j = 0; j < TAMANNO_MUNDO; j++)
         {
-            if(mundo[i +j * TAMANNO_MUNDO] != NULL) //FIXME esta busqueda se puede optimizar si guardamos las posiciones de las personas que estan en la matriz... :)
+            if (mundo[i + j * TAMANNO_MUNDO] != NULL) //FIXME esta busqueda se puede optimizar si guardamos las posiciones de las personas que estan en la matriz... :)
             {
-                Persona* persona_tmp =  mundo[i +j * TAMANNO_MUNDO];
-                
-                int flag = 0;
-                do //Este bucle se encarga de que cada persona se mueva, si no es capaz de moverse, se le asignara una nueva velocidad, para que este pueda moverse por el mundo.
-                {
-                    Tupla nueva_pos = persona_tmp->posicion;
-                    Tupla vieja_pos = persona_tmp->posicion;
+                Persona *persona_tmp = mundo[i + j * TAMANNO_MUNDO];
 
+                Tupla pos_persona = mover_persona(mundo, persona_tmp); //Movemos la persona en el mundo
 
-                    //Asignamos una velocidad aleatoria entre -VELOCIDAD_MAX y VELOCIDAD_MAX
-                    persona_tmp->velocidad.val1 = random_interval(-VELOCIDAD_MAX, VELOCIDAD_MAX);
-                    persona_tmp->velocidad.val2 = random_interval(-VELOCIDAD_MAX, VELOCIDAD_MAX);
-
-
-                    //Calculamos la nueva posicion de la persona
-                    nueva_pos.val1 = nueva_pos.val1 + persona_tmp->velocidad.val1;
-                    nueva_pos.val2 = nueva_pos.val2 +  persona_tmp->velocidad.val2;
-
-                    //Comprobamos si el movimiento es posible
-                    if((nueva_pos.val1 >= 0 && nueva_pos.val1 < TAMANNO_MUNDO) && (nueva_pos.val2 >= 0 && nueva_pos.val2 < TAMANNO_MUNDO)) //Primero comprobamos que la posicion sea valida dentro de la matriz 2D
-                    {
-                        if(mundo[nueva_pos.val1 + nueva_pos.val2 * TAMANNO_MUNDO] == NULL) //Ahora comprobamos que la casilla a moverse este vacia
-                        {
-                            persona_tmp->posicion = nueva_pos;
-                            mundo[nueva_pos.val1 + nueva_pos.val2 * TAMANNO_MUNDO] = persona_tmp;
-                            mundo[vieja_pos.val1 + vieja_pos.val2 * TAMANNO_MUNDO] = NULL;
-                            flag = 1;
-                        }
-                    }
-                } while (flag == 0);
+                if (persona_tmp->estado == INFECTADO_ASINTOMATICO || persona_tmp->estado == INFECTADO_SINTOMATICO)
+                    infectar(mundo, pos_persona);
             }
         }
     }
-    //TODO implementar radio de infeccion e infectar
 }
 
+
+void recoger_metricas(Persona** mundo)
+{
+    int cantidad_infectados = 0;
+    int i, j;
+    for (i = 0; i < TAMANNO_MUNDO; i++)
+    {
+        for (j = 0; j < TAMANNO_MUNDO; j++)
+        {
+            if (mundo[i + j * TAMANNO_MUNDO] != NULL) //FIXME esta busqueda se puede optimizar si guardamos las posiciones de las personas que estan en la matriz... :)
+            {
+                if(mundo[i + j * TAMANNO_MUNDO]->estado == INFECTADO_ASINTOMATICO || mundo[i + j * TAMANNO_MUNDO]->estado == INFECTADO_SINTOMATICO)
+                {
+                    cantidad_infectados++;
+                }
+
+            }
+        }
+    }
+
+    printf("Tamanno Poblacion : %d, Numero de infectados: %d\n",TAMANNO_POBLACION, cantidad_infectados);
+    if(TAMANNO_POBLACION == cantidad_infectados)
+    {
+        printf("Bye Bye Raccoon City\n");
+    }
+}
+void infectar(Persona **mundo, Tupla posicion_persona)
+{
+    Persona *persona_tmp = mundo[posicion_persona.val1 + posicion_persona.val2 * TAMANNO_MUNDO];
+
+    int x;
+    int y;
+    for (x = -RADIO; x <= RADIO; x++)
+    {
+        for (y = -RADIO; y <= RADIO; y++)
+        {
+            Tupla posible_persona = posicion_persona;
+
+            posible_persona.val1 += x;
+            posible_persona.val2 += y;
+
+            if ((posible_persona.val1 >= 0 && posible_persona.val1 < TAMANNO_MUNDO) && (posible_persona.val2 >= 0 && posible_persona.val2 < TAMANNO_MUNDO))
+            {
+                if(mundo[posible_persona.val1 + posible_persona.val2 * TAMANNO_MUNDO] != NULL)
+                {
+                    //TODO tener en cuenta la gente vacunada e immune
+                    mundo[posible_persona.val1 + posible_persona.val2 * TAMANNO_MUNDO]->estado = INFECTADO_SINTOMATICO;
+                }
+            }
+            
+        }
+    }
+}
+Tupla mover_persona(Persona **mundo, Persona *persona)
+{
+    int flag = 0;
+    do //Este bucle se encarga de que cada persona se mueva, si no es capaz de moverse, se le asignara una nueva velocidad, para que este pueda moverse por el mundo.
+    {
+        Tupla nueva_pos = persona->posicion;
+        Tupla vieja_pos = persona->posicion;
+
+        //Asignamos una velocidad aleatoria entre -VELOCIDAD_MAX y VELOCIDAD_MAX
+        persona->velocidad.val1 = random_interval(-VELOCIDAD_MAX, VELOCIDAD_MAX);
+        persona->velocidad.val2 = random_interval(-VELOCIDAD_MAX, VELOCIDAD_MAX);
+
+        //Calculamos la nueva posicion de la persona
+        nueva_pos.val1 = nueva_pos.val1 + persona->velocidad.val1;
+        nueva_pos.val2 = nueva_pos.val2 + persona->velocidad.val2;
+
+        //Comprobamos si el movimiento es posible
+        if ((nueva_pos.val1 >= 0 && nueva_pos.val1 < TAMANNO_MUNDO) && (nueva_pos.val2 >= 0 && nueva_pos.val2 < TAMANNO_MUNDO)) //Primero comprobamos que la posicion sea valida dentro de la matriz 2D
+        {
+            if (mundo[nueva_pos.val1 + nueva_pos.val2 * TAMANNO_MUNDO] == NULL) //Ahora comprobamos que la casilla a moverse este vacia
+            {
+                persona->posicion = nueva_pos;
+                mundo[nueva_pos.val1 + nueva_pos.val2 * TAMANNO_MUNDO] = persona;
+                mundo[vieja_pos.val1 + vieja_pos.val2 * TAMANNO_MUNDO] = NULL;
+                flag = 1;
+                return nueva_pos;
+            }
+        }
+    } while (flag == 0);
+}
 void inicializar_mundo(Persona **mundo)
 {
     int i, j, k;
@@ -178,26 +241,24 @@ void inicializar_mundo(Persona **mundo)
         }
     }
 
-
-
     k = 0; //Usado para indexar el vector de las posiciones
     Tupla posiciones_asignadas[TAMANNO_POBLACION] = {{0, 0}};
     for (i = 0; i < TAMANNO_POBLACION; i++)
     {
         Persona *persona_tmp = malloc(sizeof(Persona));
-        persona_tmp->edad = random_interval(0,110);
+        persona_tmp->edad = random_interval(0, 110);
 
-        #ifdef DEBUG
+#ifdef DEBUG
         persona_tmp->identificador = i + 1;
-        #endif
+#endif
 
         Tupla pos;
 
         int flag = 0; // 0 si no existe nadie en esa posicion, 1 en el caso contrario.
         do
         {
-            pos.val1 = random_interval(0,TAMANNO_MUNDO -1);
-            pos.val2 = random_interval(0,TAMANNO_MUNDO -1);
+            pos.val1 = random_interval(0, TAMANNO_MUNDO - 1);
+            pos.val2 = random_interval(0, TAMANNO_MUNDO - 1);
 
             flag = comprobar_posicion(posiciones_asignadas, pos); //Comprobamos si existe algun otra persona en esa misma posicion.
         } while (flag != 0);
@@ -205,7 +266,7 @@ void inicializar_mundo(Persona **mundo)
         posiciones_asignadas[k] = pos;
         k++;
         persona_tmp->posicion = pos;
-        
+
         if (i == 0) //Elegimos el paciente 0
         {
             persona_tmp->estado = INFECTADO_SINTOMATICO;
@@ -239,19 +300,18 @@ void dibujar_mundo(Persona **mundo)
         {
             if (mundo[i + j * TAMANNO_MUNDO] != NULL)
             {
-                if (mundo[i + j * TAMANNO_MUNDO]->estado == INFECTADO_SINTOMATICO)
+                if (mundo[i + j * TAMANNO_MUNDO]->estado == INFECTADO_SINTOMATICO || mundo[i + j * TAMANNO_MUNDO]->estado == INFECTADO_ASINTOMATICO)
                     printf("X      ");
                 else
                 {
-                    #ifdef DEBUG
-                    printf("%d      ",mundo[i + j * TAMANNO_MUNDO]->identificador);
-                    #endif
+#ifdef DEBUG
+                    printf("%d      ", mundo[i + j * TAMANNO_MUNDO]->identificador);
+#endif
 
-                    #ifndef DEBUG
+#ifndef DEBUG
                     printf("1      ");
-                    #endif
+#endif
                 }
-
             }
             else
             {
@@ -262,12 +322,9 @@ void dibujar_mundo(Persona **mundo)
     }
 }
 
-
 int random_interval(int min, int max)
 {
-   return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+    return min + rand() / (RAND_MAX / (max - min + 1) + 1);
 }
-
-
 
 //Si has llegado hasta aqui, te has ganado una galleta 🍪
